9b6c2879d7d2cf3e3057ad5d8898c435b2e6bfcd
Stefano Rosarin <stefanorosarin94@gmail.com>
2026-01-08 18:31:58 +0100

    Update extended_gcd.py

--- extended_gcd.py
+++ extended_gcd.py
@@ -1,1 +1,8 @@
-ciao
+"""
+Basic number-theoretic functions used in cryptography and number theory.
+
+This module contains simple and well-documented implementations
+of the Extended Euclidean algorithm to compute the GCD of two integers
+and the Bézout coefficients. 
+"""
+
