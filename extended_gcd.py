4656b48516ba038367466b10577d72ed83309e1e
Stefano Rosarin <stefanorosarin94@gmail.com>
2026-02-07 10:38:04 +0100

    Update extended_gcd.py

--- extended_gcd.py
+++ extended_gcd.py
@@ -6,3 +6,17 @@ of the Extended Euclidean algorithm to compute the GCD of two integers
 and the Bézout coefficients. 
 """
 
+def extended_gcd(a: int, b: int) ->
+    u, g, v, y = 1, a, 0, b
+    while y !=0:
+      u, g, v, y = v, y, u-(g//y)*v, a%b
+    return (g,u,v)
+
+if __name__ == "__main__":
+    print(gcd(48, 18))   # expected: (6,-1,3)
+    print(gcd(17, 5))    # expected: (1,-2,7)
+    print(gcd(0, 7))     # expected: (7,0,1)
+
+
+
+    
