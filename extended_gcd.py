09d3e53a001b1e386fc1893a60bb7241412377b7
StefanoRosarin <stefanorosarin94@gmail.com>
2026-02-07 11:42:18 +0100

    Update extended_gcd.py

--- extended_gcd.py
+++ extended_gcd.py
@@ -6,16 +6,44 @@ of the Extended Euclidean algorithm to compute the GCD of two integers
 and the Bézout coefficients. 
 """
 
-def extended_gcd(a: int, b: int) -> tuple[int, int, int]
+def extended_gcd(a: int, b: int) -> tuple[int, int, int]:
+    """
+    Compute the greatest common divisor g of a and b together with the Bézout coefficient (u,v) for the relation ua+vb=g.
+
+    Parameters
+    ----------
+    a : int
+        An Integer.
+    b : int
+        An Integer.
+
+    Returns
+    -------
+    (g,u,v)
+     g: int
+        The greatest common divisor of a and b (always non-negative).
+     u: int
+        The Bézout coefficient for a.
+     v: int 
+        The Bézout coefficient for b.
+    """
+
     u, g, v, y = 1, a, 0, b
     while y !=0:
-      u, g, v, y = v, y, u-(g//y)*v, a%b
-    return (abs(g),u,v)
+      u, g, v, y = v, y, u-(g//y)*v, g%y
+    
+    # Ensure GCD is non-negative.
+    if g<0:
+        g=-g
+        u=-u
+        v=-v
+    
+    return (g,u,v)
 
 if __name__ == "__main__":
-    print(gcd(48, 18))   # expected: (6,-1,3)
-    print(gcd(17, 5))    # expected: (1,-2,7)
-    print(gcd(0, 7))     # expected: (7,0,1)
+    print(extended_gcd(48, 18))   # expected: (6,-1,3)
+    print(extended_gcd(17, 5))    # expected: (1,-2,5)
+    print(extended_gcd(0, 7))     # expected: (7,0,1)
 
 
 
